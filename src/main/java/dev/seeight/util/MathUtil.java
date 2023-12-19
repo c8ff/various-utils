2e497cbd0a833375ab6ae622322cba9edabe7c7a
C8FF <theprestigefox@googlemail.com>
2023-12-18 19:37:08 -0600

    Add 'digitsOf' to MathUtil

--- src/main/java/dev/seeight/util/MathUtil.java
+++ src/main/java/dev/seeight/util/MathUtil.java
@@ -83,4 +83,31 @@ public class MathUtil {
 	public static double getRandomDouble(double min, double max, double random) {
 		return random * (max - min - 1) + min;
 	}
+
+	// From: https://stackoverflow.com/a/51099524
+	public static int digitsOf(int n) {
+		if (n >= 0) {
+			if (n < 10) return 1;
+			if (n < 100) return 2;
+			if (n < 1000) return 3;
+			if (n < 10000) return 4;
+			if (n < 100000) return 5;
+			if (n < 1000000) return 6;
+			if (n < 10000000) return 7;
+			if (n < 100000000) return 8;
+			if (n < 1000000000) return 9;
+			return 10;
+		} else {
+			if (n > -10) return 2;
+			if (n > -100) return 3;
+			if (n > -1000) return 4;
+			if (n > -10000) return 5;
+			if (n > -100000) return 6;
+			if (n > -1000000) return 7;
+			if (n > -10000000) return 8;
+			if (n > -100000000) return 9;
+			if (n > -1000000000) return 10;
+			return 11;
+		}
+	}
 }
